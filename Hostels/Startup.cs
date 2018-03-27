17f7deb881203bd315e458938c0b5957ebf413a0
Jcardif <josh.cardif14@outlook.com>
2018-03-27 12:52:46 +0300

    Add project files.

---
+++ Hostels/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Hostels.Startup))]
+
+namespace Hostels
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
